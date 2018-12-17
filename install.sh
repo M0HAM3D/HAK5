74a72f26bbb09baaa295fd6d038cab8de8bfc8db
Mohamed Salama <45932160+M0HAM3D@users.noreply.github.com>
2018-12-17 10:42:53 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -32,6 +32,7 @@ if [ "$ins" = "2" ];then
 	pip install --upgrade pip
 	mv model .model
 	cd .model
+	chmod +x *
 	mv .mohamed /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/
 	cd .. 
 	termux-setup-storage
@@ -41,9 +42,13 @@ if [ "$ins" = "2" ];then
 	bash HAK5.sh
 elif [ "$ins" = "1" ]; then 
 	mv model .model
+	cd .model
+	chmod +x *
+	cd ..
 	mkdir HAK5_PaYLOad
 	chmod +x HAK5.sh
 	chmod 700 HAK5.sh
+	cd ~
 	sudo apt-get install toilet -y
 	sudo apt-get install wget -y
 	sudo apt-get install figlet -y
